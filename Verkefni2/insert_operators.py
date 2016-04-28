a2a3506d5136f6067ff24d8e051a7829ad928348
Darri Steinn Konradsson <darri15@hotmail.com>
2016-04-28 16:27:46 +0000

    finished insert_operators and therefore finished project 2 and week 1

--- Verkefni2/insert_operators.py
+++ Verkefni2/insert_operators.py
@@ -1,30 +1,38 @@
+from itertools import combinations
+from itertools import product
+
+def concatenate(s):
+    n = len(s)
+    comb = []
+    for i in range(0, n):
+        for j in combinations(range(1, n), i):
+            comb.append(j)
+    lis = []
+    for i, e in enumerate(comb):
+        e = (0,) + e
+        lis.append([])
+        for j in range(len(e)-1):
+            lis[i].append(int(''.join(map(str, s[e[j]:e[j+1]]))))
+        lis[i].append(int(''.join(map(str, s[e[len(e)-1]:]))))
+    return lis
+
+def operations(n):
+    return list(product(['+', '-'], repeat=n))
+
 def insert_operators(eqn, target):
-    # +-concatenate
-    combinations = []
-    myList = []
-    cnt = 1
-    while 1:
-        per = concatenate(eqn, cnt)
-        cnt += 1
-        combinations.append(per)
-        if len(per) == 1:
-            break
-    #for i, n in enumerate(combinations):
-
-
-    return combinations
-
-def concatenate(lis, n):
-    res = []
-    isFull = False
-    for i in range(0, len(lis)):
-        s = ''
-        for j in range(n):
-            if j + i == len(lis):
-                isFull = True
-                break
-            s += str(lis[i+j])
-        if isFull:
-            break
-        res.append(s)
-    return res
+    lis = concatenate(eqn)
+    for i, e in enumerate(lis):
+        oper = operations(len(e)-1)
+        for j, o in enumerate(oper):
+            summ = e[0]
+            s = str(e[0])
+            for k, p in enumerate(o):
+                if p == '-':
+                    summ -= e[k+1]
+                    s = '{0}-{1}'.format(s, str(e[k+1]))
+                else:
+                    summ += e[k+1]
+                    s = '{0}+{1}'.format(s, str(e[k+1]))
+            if summ == target:
+                return '{0}={1}'.format(s, str(target))
+    return None
