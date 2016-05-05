70b429e190476c2d5901aec0430eb0bbfd060205
Darri Steinn Konradsson <darri15@hotmail.com>
2016-05-05 22:14:54 +0000

    Think Im done

---
+++ Verkefni4/README.md
@@ -0,0 +1,20 @@
+This application sorts your downloads directory. You should run this app in a shell with two flags, i.e. -i (or --input) which is the downloads directory (full path not needed) and -c (or --config) which is a json config file that helps the sorting.
+e.g.:
+./download_clean_up.py -i downloads/ -c config.json
+
+You are provided with a default config.json file that has the following attributes:
+"NonValidFileExtensions" -> this will be file extensions that should be removed (Warning zone)
+"ValidSubtitleFileExtensions" -> these are the file extensions of your subtitles
+"ValidBookFileExtensions" -> all valid book file extensions
+"ValidSongFileExtensions" -> all valid song file extensions
+"ValidVideoFileExtensions" -> all valid video file extensions
+"ExcludeDirectories" -> these are the private directories that should not be sorted
+"KnownTVShows" -> this will help the application to sort your files based on known TV-Shows. Feel free to add your TV-Shows (not necessary).
+
+No PyPI packages are needed, nor other dependencies.
+
+Furthermore you can run:
+./download_clean_up.py -h
+for further information.
+
+Good luck in keeping your downloads directory clean.
--- Verkefni4/download_clean_up.py
+++ Verkefni4/download_clean_up.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
-import argparse, re, os, sys, json
+import argparse, re, os, sys, json, logging
 
-def re_type(s):
-    try:
-        re.compile(s)
-    except:
-        raise argparse.ArgumentTypeError('Invalid regex')
-    return s
+# keep a global LOG of all crucial changes
+logging.basicConfig(level=logging.INFO,
+        filename='info_logger.log',
+        filemode='a',
+        format='%(asctime)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+LOGGER = logging.getLogger('info_logger')
 
 def move_files(filename, destination):
     try:
         if os.path.exists(filename):
             os.rename(filename, destination)
+            LOGGER.info(': File MOVED!:\n{0} -> {1}\n'.format(filename, destination))
     except:
-        sys.stderr.write('Could not rename/move {0} to {1}'.format(filename, directory))
+        sys.stderr.write('Could not rename/move {0} to {1}'.format(filename, destination))
 
 def create_directories(directory):
     try:
@@ -22,81 +24,152 @@ def create_directories(directory):
     except:
         sys.stderr.write('Could not create directory {0}'.format(directory))
 
-def initialize(ROOT):
-    create_directories(os.path.join(ROOT, 'Movies'))
-    create_directories(os.path.join(ROOT, 'Songs'))
-    create_directories(os.path.join(ROOT, 'Books'))
-    create_directories(os.path.join(ROOT, 'TV-Shows'))
-    create_directories(os.path.join(ROOT, 'Other (unsorted)'))
-
 def remove_directory(directory):
-    if os.path.isdir(filename):
+    if os.path.isdir(directory):
         try:
-            if os.listdir(filename) == []:
-                os.rmdir(filename)
+            if os.listdir(directory) == []:
+                os.rmdir(directory)
         except:
-            sys.stderr.write('Could not remove the directory {0}'.format(filename))
+            sys.stderr.write('Could not remove the directory {0}'.format(directory))
 
 def remove_file(filename):
     try:
         if os.path.exists(filename):
             os.remove(filename)
+            LOGGER.info(': File REMOVED!:\n{0}\n'.format(filename))
     except:
         sys.stderr.write('Could not remove file {0}'.format(filename))
 
+#
+# get the user configurations from the config file
+#
 def get_configurations(PATH):
-    with open(PATH) as cf:
-        items = json.loads(cf.read())
-        return {'NonValid': items['NonValidFileExtensions'],
-                'Subtitles': items['ValidSubtitleFileExtensions'],
-                'Books': items['ValidBookFileExtensions'],
-                'Exclude': items['ExcludeDirectories'],
-                'Video': items['ValidVideoFileExtensions'],
-                'Songs': items['ValidSongFileExtensions']}
+    try:
+        with open(PATH) as cf:
+            items = json.loads(cf.read())
+            return {'NonValid': items['NonValidFileExtensions'],
+                    'Subtitles': items['ValidSubtitleFileExtensions'],
+                    'Books': items['ValidBookFileExtensions'],
+                    'Exclude': items['ExcludeDirectories'],
+                    'Video': items['ValidVideoFileExtensions'],
+                    'KnownShows': items['KnownTVShows'],
+                    'Songs': items['ValidSongFileExtensions']}
+    except:
+        sys.stderr.write('Could not open the configuration file, or json file not valid\n')
 
-def check_file_extension(f, key):
-    return any(f for x in key if f.endswith(x))
+#
+# get all known shows that the user specified in the config file
+#
+def is_a_known_show(f, keys):
+    folder = [x for x in keys if x.title() in f.title()]
+    if not folder:
+        return []
+    else:
+        return folder[0]
 
+#
+# create title of the folder being created
+#
+def get_title(f):
+    return ' '.join(f[:f.rfind('.')].split('.')).strip().title()
+
+def check_file_extension(f, keys):
+    return any(f for x in keys if f.endswith(x))
+
+def check_file_start(f, path, keys):
+    return any(f for x in keys if f.startswith(os.path.join(path, x)))
+
+#
+# initialize the downloads directory with few root directories
+#
+def initialize(ROOT):
+    create_directories(os.path.join(ROOT, 'Movies'))
+    create_directories(os.path.join(ROOT, 'Songs'))
+    create_directories(os.path.join(ROOT, 'Books'))
+    create_directories(os.path.join(ROOT, 'TV-Shows'))
+    create_directories(os.path.join(ROOT, 'Other (Unsorted)'))
+
+#
+# sort the files and clean up the downloads directory
+#
 def sort_files(args):
-    TV_RE = r'(.*)(?: ?s ?| ?season ?| ?\[ ?|\.)(\d+)(?: ?e ?| ?episode ?| ?x ?)(\d+)'
+    TV_RE = r'(.*)(?:s|season| |\[)( ?\d+)(?: *?| ?-? ?)(?:e|episode|x)( ?\d+)'
     TV_CRE = re.compile(TV_RE, re.I)
     config = get_configurations(args.config_filename)
     initialize(args.filename)
+    ROOT = args.filename[args.filename.rfind('/'):]
     for root, dirs, files in os.walk(args.filename, topdown=False):
+        if check_file_start(root, args.filename, config['Exclude']):
+            continue
         for f in files:
-            tv_show = TV_CRE.findall(f)
+            tv_show = TV_CRE.findall(' '.join(f.split('.')))
             path = os.path.join(root, f)
-            #print(os.path.dirname(path))
+            old_parent_folder = re.sub(args.filename + '/', '', path)
+            index = old_parent_folder.find('/')
+            if index == -1:
+                index = len(old_parent_folder)
+            old_parent_folder = old_parent_folder[:index]
+            known_show = is_a_known_show(old_parent_folder, config['KnownShows'])
             if check_file_extension(f, config['NonValid']):
-                # delete file and continue
+                # delete non valid files
                 remove_file(path)
-            elif check_file_extension(f, config['Songs']):
-                parent_folder = '{0}{1}'.format('Songs/',
-                        ' '.join(f[:f.rfind('.')].split('.')).strip())
+            elif not known_show == []:
+                # if the show is in the config file, then it is easier to clean up
+                parent_folder = '{0}{1}'.format('TV-Shows/', known_show)
                 create_directories(os.path.join(args.filename, parent_folder))
-                move_files(path, os.path.join(args.filename, parent_folder + '/' + f))
+                new_path = os.path.join(args.filename, parent_folder)
+                if os.listdir(new_path) == []:
+                    move_files(os.path.join(args.filename, old_parent_folder),
+                            new_path)
+                else:
+                    move_files(os.path.join(args.filename, old_parent_folder),
+                            new_path + '/' + old_parent_folder)
             elif tv_show:
-                parent_folder = '{0}{1}{2}{3}'.format('TV-Shows/',
+                # check first if we can sort tv shows from a pattern of (seasons/episodes)
+                top_folder = 'TV-Shows/'
+                if check_file_extension(f, config['Songs']):
+                    top_folder = 'Songs/'
+                parent_folder = '{0}{1}{2}{3}'.format(top_folder,
                         ' '.join(tv_show[0][0].split('.')).strip().title(),
                         '/Season ' + str(int(tv_show[0][1])),
                         '/Episode ' + str(int(tv_show[0][2])))
+                # add the subtitles to a subtitle directory for the show
+                if check_file_extension(f, config['Subtitles']):
+                    parent_folder = '{0}/Subtitles'.format(parent_folder)
+                create_directories(os.path.join(args.filename, parent_folder))
+                move_files(path, os.path.join(args.filename, parent_folder + '/' + f))
+            elif check_file_extension(f, config['Songs']):
+                parent_folder = root[root.rfind(ROOT)+len(ROOT):]
+                if parent_folder == '':
+                    parent_folder = get_title(f)
+                parent_folder = 'Songs/{0}'.format(parent_folder)
+                create_directories(os.path.join(args.filename, parent_folder))
+                move_files(path, os.path.join(args.filename, parent_folder + '/' + f))
+            elif check_file_extension(f, config['Subtitles']):
+                parent_folder = '{0}{1}{2}'.format('Movies/', get_title(f), '/Subtitles')
+                create_directories(os.path.join(args.filename, parent_folder))
+                move_files(path, os.path.join(args.filename, parent_folder + '/' + f))
+            elif check_file_extension(f, config['Books']):
+                parent_folder = '{0}{1}'.format('Books/', get_title(f))
                 create_directories(os.path.join(args.filename, parent_folder))
                 move_files(path, os.path.join(args.filename, parent_folder + '/' + f))
             elif check_file_extension(f, config['Video']):
-                parent_folder = '{0}{1}'.format('Movies/',
-                        ' '.join(f[:f.rfind('.')].split('.')).strip())
+                parent_folder = '{0}{1}'.format('Movies/', get_title(f))
                 create_directories(os.path.join(args.filename, parent_folder))
                 move_files(path, os.path.join(args.filename, parent_folder + '/' + f))
             else:
-                parent_folder = '{0}{1}'.format('Other (unsorted)/',
-                        ' '.join(f[:f.rfind('.')].split('.')).strip())
+                # if we can't find a pattern to sort, add to the Other (unsorted) directory
+                parent_folder = '{0}{1}'.format('Other (Unsorted)/', get_title(f))
                 create_directories(os.path.join(args.filename, parent_folder))
                 move_files(path, os.path.join(args.filename, parent_folder + '/' + f))
 
-        if root == args.filename:
-            for d in dirs:
-                remove_directory(d)
+        for d in dirs:
+            # remove all empty directories
+            remove_directory(os.path.join(root, d))
 
+#
+# need to validate input files from the user
+#
 def validate_file(parser, filepath):
     if os.path.exists(filepath):
         return filepath
@@ -121,6 +194,8 @@ def main():
     print('Processing...')
     try:
         sort_files(args)
+        move_files(os.path.dirname(os.path.realpath(__file__)) + '/info_logger.log',
+                args.filename + '/info_logger.log')
         print('Succeeded!')
     except:
         sys.stderr.write('Error while cleaning up the downloads directory!')
