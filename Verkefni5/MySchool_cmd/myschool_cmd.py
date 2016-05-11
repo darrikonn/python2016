45c289c77666535e68e99ed21645e7ae0384a537
Darri Steinn Konradsson <darri15@hotmail.com>
2016-05-11 16:52:11 +0000

    next is to submit assignments

--- Verkefni5/MySchool_cmd/myschool_cmd.py
+++ Verkefni5/MySchool_cmd/myschool_cmd.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
-import argparse, requests, getpass, os, sys, traceback, prettytable
+import argparse, requests, getpass, os, sys, traceback, prettytable, re
 from bs4 import BeautifulSoup
-#from prettytable import PrettyTable
+# tabulate
 
+#
+# public variables
+#
 USERNAME = 'darrik13'
 PASSWORD_FILE_NAME = '.password.txt'
+
+#
+# public enum classes
+#
+class Season:
+    Winter = 0
+    Spring = 1
+    Summer = 2
+    Autumn = 3
+
+class Grades:
+    All = 2
+    Summary = 3
+    Abstract = 4
 
 class Password:
     def __init__(self):
@@ -37,62 +54,119 @@ class MySchool:
     def __init__(self, pwd):
         self._pwd = pwd
 
-    def get_timetable(self):
+    def _get_table(self, link, c_index, t_index):
         try:
-            resp = requests.get('https://myschool.ru.is/myschool/?Page=Exe&ID=3.2',
+            resp = requests.get(link,
                     auth=(USERNAME, self._pwd))
             soup = BeautifulSoup(resp.text, 'html.parser')
-            tr_soup = soup('center')[0].table.tbody('tr')[1:-1]
-
-            # need to build my hotmail
-            # table head
-            table = '<table><thead>'
-            for tr in tr_soup[1:3]:
-                table = '{0}<tr>'.format(table)
-                for th in tr.get_text().splitlines()[1:]:
-                    table = '{0}<th>{1}</th>'.format(table, th)
-                table = '{0}</tr>'.format(table)
-            table = '{0}</thead><body>'.format(table)
-
-            # table body
-            for tr in tr_soup[3:-1]:
-                table = '{0}<tr>'.format(table)
-                for td in tr.get_text().splitlines()[1:]:
-                    table = '{0}<th>{1}</th>'.format(table, td)
-                table = '{0}</tr>'.format(table)
-            table = '{0}</tbody></table>'.format(table)
+            tr_soup = soup('center')[c_index].table.tbody('tr')[t_index:-1]
             table = '<table><tbody>{0}</tbody></table>'.format(get_string_format(tr_soup))
             return prettytable.from_html(table)
         except:
             traceback.print_exc()
-            sys.stderr.write('Could not get the timetable\n')
+            sys.stderr.write('Could not get table\n')
 
-    def get_next_assignments(self):
-            resp = requests.get('https://myschool.ru.is/myschool/?Page=Exe&ID=1.12',
-                    auth=(USERNAME, self._pwd))
-            soup = BeautifulSoup(resp.text, 'html.parser')
-            tr_soup = soup('center')[0].table.tbody('tr')[:-1]
-            table = '<table><tbody>{0}</tbody></table>'.format(get_string_format(tr_soup))
-            return prettytable.from_html(table)
+    def _get_assignment_course_ID(self, course, assignment):
+        resp = requests.get('https://myschool.ru.is/myschool/?Page=Exe&ID=1.12',
+            auth=(USERNAME, self._pwd))
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        tr_soup = soup('center')[0].table.tbody('tr')[1:-1]
+        href = ''
+        for tr in tr_soup:
+            s = tr.get_text().lower()
+            if course.lower() in s and assignment.lower() in s:
+                href = tr('td')[4].a['href']
+                break
+        return href
+
+    def get_timetable(self):
+        return self._get_table('https://myschool.ru.is/myschool/?Page=Exe&ID=3.2', 0, 1)
+
+    def submit_assignment(self, course, assignment, f, comment):
+        data = {'athugasemdnemanda': comment}
+        try:
+            T = r'(?:verkID=)(\d+)(?:.*)(?:fagid=)(\d+)'
+            TC = re.compile(T)
+            href = self._get_assignment_course_ID(course, assignment)
+            if href == '':
+                raise NoCourseOrAssignment
+            ids = TC.findall(href)
+            link = 'https://myschool.ru.is/myschool/?Page=LMS&ID=16&fagID={0}&View=52&ViewMode=2&Tab=&Act=11&verkID={1}'.format(ids[0][1],
+                    ids[0][0])
+            resp = requests.post(link, data=data, auth=(USERNAME, self._pwd))
+        except NoCourseOrAssignment:
+            sys.stderr.write('No course or assignment with that name!\n')
+        except:
+            traceback.print_exc()
+            sys.stderr.write('Could not submit the assignment')
+
+    def get_assignments(self, filt):
+        resp = requests.get('https://myschool.ru.is/myschool/?Page=Exe&ID=1.12',
+            auth=(USERNAME, self._pwd))
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        tr_soup = soup('center')[0].table.tbody('tr')[:-1]
+        table = '<table><thead>{0}</thead><tbody>'.format(tr_soup[0])
+        for i in range(1, len(tr_soup)):
+            if filt.lower() in tr_soup[i].get_text().lower():
+                table = '{0}{1}'.format(table, tr_soup[i])
+        table = '{0}</tbody></table>'.format(table)
+        return prettytable.from_html(table)
+
+    def get_courses(self, year, season):
+        return self._get_table('https://myschool.ru.is/myschool/?Page=Exe&ID=7&Tab=1&Sem={0}{1}'.format(year,
+                    getattr(Season, season.title())), 0, 0)
 
     def get_examtable(self):
-            resp = requests.get('https://myschool.ru.is/myschool/?Page=Exe&ID=3.3&Tab=1',
-                    auth=(USERNAME, self._pwd))
-            soup = BeautifulSoup(resp.text, 'html.parser')
-            tr_soup = soup('center')[0].table.tbody('tr')[:-1]
-            table = '<table><tbody>{0}</tbody></table>'.format(get_string_format(tr_soup))
-            return prettytable.from_html(table)
+        return self._get_table('https://myschool.ru.is/myschool/?Page=Exe&ID=3.3&Tab=1', 0, 0)
 
-    def get_all_assignments(self):
-            resp = requests.get('https://myschool.ru.is/myschool/?Page=Exe&ID=1.12',
+    def get_new_material(self):
+        return self._get_table('https://myschool.ru.is/myschool/?Page=Exe&ID=1.17', 0, 0)
+
+    def get_grades(self, command):
+        return self._get_table('https://myschool.ru.is/myschool/?Page=Exe&ID=1.14',
+                getattr(Grades, command.title()), 0)
+
+    def get_online_quizzes(self):
+        return self._get_table('https://myschool.ru.is/myschool/?Page=Exams&ID=13', 1, 0)
+
+    def get_groups(self, year, season):
+        return self._get_table('https://myschool.ru.is/myschool/?Page=Exe&ID=1.11&Tab=2&Sem={0}{1}'.format(year,
+                    getattr(Season, season.title())), 1, 0)
+
+    def get_username(self):
+        resp = requests.get('https://myschool.ru.is/myschool/?Page=Front',
+                auth=(USERNAME, self._pwd))
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        return soup.div.table('tr')[3].td.div.span.get_text()
+
+    def get_book_list(self):
+        try:
+            resp = requests.get('https://myschool.ru.is/myschool/?Page=Exe&ID=1.13',
                     auth=(USERNAME, self._pwd))
             soup = BeautifulSoup(resp.text, 'html.parser')
-            tr_soup = soup('center')[1].table.tbody('tr')[:-1]
-            table = '<table><tbody>{0}</tbody></table>'.format(get_string_format(tr_soup))
+            table_soup = soup('center')
+            table = ''
+            for t in table_soup:
+                thead = '<thead>'
+                tbody = '<tbody>'
+                for i, tr in enumerate(t.find_all('tr')):
+                    if i == 0:
+                        thead = '{0}{1}'.format(thead, tr)
+                    else:
+                        tbody = '{0}<tr>{1}</tr>'.format(tbody,
+                                get_string_format(tr.find_all('td')[1:-1]))
+                table = '{0}<table>{1}</thead>{2}</tbody></table>'.format(table, thead, tbody)
             return prettytable.from_html(table)
+        except:
+            traceback.print_exc()
+            sys.stderr.write('Could not get new material\n')
 
 def get_string_format(s):
     return ''.join(str(x) for x in s)
+
+def print_table(table):
+    for t in table:
+        print(t)
 
 def main():
     # user has to have sudo privileges because of sensitive password information
@@ -104,15 +178,28 @@ def main():
     parser.add_argument('-tt', '--timetable', dest='timetable',
             help='This command will list your timetable of your courses that you\'re taking',
             action='store_true')
-    parser.add_argument('-na', '--next_assignments', dest='next_assignments',
-            help='This command will list your next assignments that are due in the future',
-            action='store_true')
-    parser.add_argument('-aa', '--all_assignments', dest='all_assignments',
-            help='This command will list all your assignments that are past due date',
-            action='store_true')
     parser.add_argument('-et', '--examtable', dest='examtable',
-            help='This command will list all your exams in a table',
+            help='This command will list all your exams',
             action='store_true')
+    parser.add_argument('-nm', '--new_material', dest='new_material',
+            help='This command will list all new materials',
+            action='store_true')
+    parser.add_argument('-bl', '--booklist', dest='booklist',
+            help='This command will list all your books',
+            action='store_true')
+    parser.add_argument('-q', '--quizzes', dest='quizzes',
+            help='This command will list all your books',
+            action='store_true')
+    parser.add_argument('-a', '--assignments', dest='assignments', nargs='*',
+            help='This command will list your next assignments that are due in the future')
+    parser.add_argument('-c', '--courses', dest='courses', nargs='*',
+            help='This command will list all your course')
+    parser.add_argument('-gr', '--groups', dest='groups', nargs='*',
+            help='This command will list all your groups')
+    parser.add_argument('-g', '--grades', dest='grades', nargs='*',
+            help='This command will list all your grades')
+    parser.add_argument('-sa', '--submit_assignment', dest='submit_assignment', nargs='*',
+            help='This command will submit your assignment')
 
     args = parser.parse_args()
 
@@ -120,21 +207,47 @@ def main():
     ms = MySchool(pwd.get_password())
 
     if args.timetable:
-        timetable = ms.get_timetable()
-        for t in timetable:
-            print(t)
+        print_table(ms.get_timetable())
     elif args.examtable:
-        examtable = ms.get_examtable()
-        for e in examtable:
-            print(e)
-    elif args.next_assignments:
-        assignments = ms.get_next_assignments()
-        for a in assignments:
-            print(a)
-    elif args.all_assignments:
-        assignments = ms.get_all_assignments()
-        for a in assignments:
-            print(a)
+        print_table(ms.get_examtable())
+    elif not args.assignments == None:
+        filt = ''
+        if len(args.assignments) > 0:
+            filt = args.assignments[0]
+        print_table(ms.get_assignments(filt))
+    elif args.new_material:
+        print_table(ms.get_new_material())
+    elif args.quizzes:
+        print_table(ms.get_online_quizzes())
+    elif not args.submit_assignment == None:
+        try: # need to fix, start with both comment and file
+            if not len(args.submit_assignment) == 4:
+                sys.stderr('Arguments not fulfilled, size needs to be of length 4!\n')
+                return
+            ms.submit_assignment(args.submit_assignment[0], args.submit_assignment[1],
+                    args.submit_assignment[2], args.submit_assignment[3])
+            print('Success!')
+        except:
+            sys.stderr('Could not submit the assignment')
+    elif not args.grades == None:
+        if hasattr(Grades, args.grades[0].title()):
+            print_table(ms.get_grades(args.grades[0]))
+        else:
+            sys.stderr('Command is not valid, "{0}". Valid commands are "all", "summary", "abstract"\n'.format(season))
+    elif args.booklist:
+        print_table(ms.get_book_list())
+    elif not args.groups == None:
+        if hasattr(Season, args.groups[1].title()):
+            print_table(ms.get_groups(args.groups[0], args.groups[1]))
+        else:
+            sys.stderr('Season is not valid, "{0}". Valid seasons are "spring", "autumn", "summer", "winter"\n'.format(args.groups[1]))
+    elif not args.courses == None:
+        if hasattr(Season, args.courses[1].title()):
+            print_table(ms.get_courses(args.courses[0], args.courses[1]))
+        else:
+            sys.stderr('Season is not valid, "{0}". Valid seasons are "spring", "autumn", "summer", "winter"\n'.format(args.courses[1]))
+    else:
+        print('Logged in as: {0}'.format(ms.get_username()))
 
 if __name__ == '__main__':
     sys.exit(main())
